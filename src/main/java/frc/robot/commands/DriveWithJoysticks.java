fd0ff5cc758b0986adeaf5278b48caee00e4e1b2
lukem232 <14318745+lukemagdalin@users.noreply.github.com>
2022-08-17 20:56:47 -0400

    Add joystick value processing

--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -7,8 +7,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.util.GeomUtil;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
 import java.util.function.Supplier;
 
 public class DriveWithJoysticks extends CommandBase {
@@ -17,16 +22,20 @@ public class DriveWithJoysticks extends CommandBase {
   private final Supplier<Double> leftXSupplier;
   private final Supplier<Double> leftYSupplier;
   private final Supplier<Double> rightYSupplier;
+  private final Supplier<Boolean> robotRelativeOverride;
 
   private static final double deadband = 0.1;
 
   /** Creates a new DriveWithJoysticks. */
-  public DriveWithJoysticks(Drive drive, Supplier<Double> leftXSupplier, Supplier<Double> leftYSupplier, Supplier<Double> rightYSupplier) {
+  public DriveWithJoysticks(Drive drive, Supplier<Double> leftXSupplier,
+      Supplier<Double> leftYSupplier, Supplier<Double> rightYSupplier,
+      Supplier<Boolean> robotRelativeOverride) {
     addRequirements(drive);
     this.drive = drive;
     this.leftXSupplier = leftXSupplier;
     this.leftYSupplier = leftYSupplier;
     this.rightYSupplier = rightYSupplier;
+    this.robotRelativeOverride = robotRelativeOverride;
   }
 
   // Called when the command is initially scheduled.
@@ -40,22 +49,40 @@ public class DriveWithJoysticks extends CommandBase {
     double leftX = leftXSupplier.get();
     double leftY = leftYSupplier.get();
     double rightY = rightYSupplier.get();
-    
+
+    // Get direction and magnitude of linear axes
+    double linearMagnitude = Math.hypot(leftX, leftY);
+    Rotation2d linearDirection = new Rotation2d(leftX, leftY);
+
     // Apply deadband
-    leftX = MathUtil.applyDeadband(leftX, deadband);
-    leftY = MathUtil.applyDeadband(leftY, deadband);
+    linearMagnitude = MathUtil.applyDeadband(linearMagnitude, deadband);
     rightY = MathUtil.applyDeadband(rightY, deadband);
 
     // Apply squaring
-    leftX = Math.copySign(leftX * leftX, leftX);
-    leftY = Math.copySign(leftY * leftY, leftY);
+    linearMagnitude =
+        Math.copySign(linearMagnitude * linearMagnitude, linearMagnitude);
     rightY = Math.copySign(rightY * rightY, rightY);
-    
+
+    // Calcaulate new linear components
+    Translation2d linearVelocity =
+        new Pose2d(new Translation2d(), linearDirection)
+            .transformBy(
+                GeomUtil.transformFromTranslation(linearMagnitude, 0.0))
+            .getTranslation();
+
     // Send to drive
-    double leftXMetersPerSec = leftX * drive.getMaxLinearSpeedMetersPerSec();
-    double leftYMetersPerSec = leftY * drive.getMaxLinearSpeedMetersPerSec();
+    double leftXMetersPerSec =
+        linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec();
+    double leftYMetersPerSec =
+        linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec();
     double rightYRadPerSec = rightY * drive.getMaxAngularSpeedRadPerSec();
-    drive.runVelocity(new ChassisSpeeds(leftXMetersPerSec, leftYMetersPerSec, rightYRadPerSec)); 
+    if (robotRelativeOverride.get()) {
+      drive.runVelocity(new ChassisSpeeds(leftXMetersPerSec, leftYMetersPerSec,
+          rightYRadPerSec));
+    } else {
+      drive.runVelocity(ChassisSpeeds.fromFieldRelativeSpeeds(leftXMetersPerSec,
+          leftYMetersPerSec, rightYRadPerSec, drive.getRotation()));
+    }
   }
 
   // Called once the command ends or is interrupted.
