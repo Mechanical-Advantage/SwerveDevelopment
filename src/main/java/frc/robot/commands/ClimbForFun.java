0b4bf0e05b0dc0a4267546529ad097f2c959a0ad
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-10-15 21:48:43 -0400

    Adjust fun climb

--- src/main/java/frc/robot/commands/ClimbForFun.java
+++ src/main/java/frc/robot/commands/ClimbForFun.java
@@ -25,12 +25,12 @@ public class ClimbForFun extends SequentialCommandGroup {
       new Translation2d(FieldConstants.hangarLength - 1.2,
           FieldConstants.fieldWidth - (FieldConstants.hangarWidth / 2)),
       Rotation2d.fromDegrees(180.0));
-  public static final double climbExtendPercent = 0.5;
-  public static final double climbExtendTime = 1.0;
+  public static final double climbExtendPercent = 1.0;
+  public static final double climbExtendTime = 2.0;
   public static final Pose2d grabPose =
       extendPose.transformBy(GeomUtil.transformFromTranslation(-0.5, 0.0));
   public static final double grabVelocity = Units.inchesToMeters(10.0);
-  public static final double climbRetractPercent = -0.5;
+  public static final double climbRetractPercent = -1.0;
   public static final double climbRetractTime = 1.0;
 
   /** Creates a new ClimbForFun. */
@@ -39,8 +39,7 @@ public class ClimbForFun extends SequentialCommandGroup {
         new AutoDrive(drive,
             List.of(Waypoint.fromHolonomicPose(AutoPosition.TARMAC_A.getPose()),
                 Waypoint.fromHolonomicPose(extendPose,
-                    extendPose.getRotation()
-                        .rotateBy(Rotation2d.fromDegrees(180.0))))),
+                    extendPose.getRotation()))),
         new StartEndCommand(() -> climber.runPercent(climbExtendPercent),
             climber::stop, climber).withTimeout(climbExtendTime),
         new AutoDrive(drive,
