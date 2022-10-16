7178b0618124332aabfdb23b6e524d453184b436
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-10-15 21:49:42 -0400

    Stop driving at end of trajectories

--- src/main/java/frc/robot/commands/AutoDrive.java
+++ src/main/java/frc/robot/commands/AutoDrive.java
@@ -179,7 +179,9 @@ public class AutoDrive extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drive.stop();
+  }
 
   // Returns true when the command should end.
   @Override
